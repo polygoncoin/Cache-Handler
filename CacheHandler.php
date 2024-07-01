b4b48ae86f23ce246225659e8c79b9d4b1084891
“polygoncoin <polygon.co.in@gmail.com>
2024-07-01 15:19:02 +0530

    Added composer.json

--- CacheHandler.php
+++ CacheHandler.php
@@ -38,7 +38,7 @@ class CacheHandler
         $fileLocation = $this->$cacheLocation . $filePath;
 
         if (!file_exists($fileLocation)) {
-            header("HTTP/1.0 404 Not Found");
+            header('HTTP/1.0 404 Not Found');
             die();
         }
 
@@ -57,7 +57,7 @@ class CacheHandler
                 @strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $modifiedTime
             )
         ) { 
-            header("HTTP/1.1 304 Not Modified"); 
+            header('HTTP/1.1 304 Not Modified'); 
             exit;
         }
         $this->serveFile($fileLocation, $modifiedTime, $eTag);
@@ -71,22 +71,19 @@ class CacheHandler
      * @param string  $eTag
      * @return void
      */
-    private function serveFile($fileLocation, $modifiedTime, $eTag) {
-        // File name requested for download
-        $fileName = basename($fileLocation);
-
+    private function serveFile($fileLocation, $modifiedTime, $eTag)
+    {
         // Get the $fileLocation file mime
         $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
         $mime = finfo_file($fileInfo, $fileLocation);
         finfo_close($fileInfo);
 
-        // send the headers
-        //header("Content-Disposition: attachment; filename='$fileName';");
+        // Headers
         header('Cache-Control: max-age=0, must-revalidate');
         header("Last-Modified: ".gmdate("D, d M Y H:i:s", $modifiedTime)." GMT"); 
-        header("Etag:\"{$eTag}\"");
+        header("Etag:'{$eTag}'");
         header('Expires: -1');
-        header("Content-Type: $mime");
+        header("Content-Type: {$mime}");
         header('Content-Length: ' . filesize($fileLocation));
 
         // Send file content as stream
